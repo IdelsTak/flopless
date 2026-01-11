d0ab74e79482128a1bced0552971fac46aa31a18
Hiram K <hiram.kamau@outlook.com>
2026-01-11 09:54:17 +0300

    Initial project setup
    Adds the basic Maven project structure and a main class.

---
+++ src/main/java/com/github/idelstak/flopless/Flopless.java
@@ -0,0 +1,13 @@
+
+package com.github.idelstak.flopless;
+
+/**
+ *
+ * @author kamau
+ */
+public class Flopless {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
